3508f139c40fe0b87cec58224ce7adada93fd1a9
TheKenkei <kenkei.message@gmail.com>
2024-08-25 16:40:12 +0300

    update configs

---
+++ aliases/docker.zsh
@@ -0,0 +1,6 @@
+alias dc="docker compose"
+alias drestart="docker compose down ; docker compose up -d && docker compose logs -f"
+
+alias de="docker exec -it "
+alias de-db="docker exec -it postgres su postgres"
+alias dl="docker logs -f "
---
+++ aliases/packages.zsh
@@ -0,0 +1,8 @@
+alias npm="npm -d"
+alias npm-clear="rm -rf ~/.npm"
+alias nr="npm run"
+alias npmi="rm -rf $HOME/.npm node_modules/ && npm i"
+alias yarni="rm -rf $HOME/.yarn/berry/ $HOME/.cache/yarn node_modules && yarn"
+
+alias br="bun run"
+alias bi="bun install "
